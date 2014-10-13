7f59c4be655e0b0412692774beaf9578a92dd6b7
Matt Bettinson <mattbettinson@me.com>
2014-10-13 13:18:16 -0400

    Added centering, titles, and breaks

--- generator.py
+++ generator.py
@@ -4,27 +4,33 @@ from PIL import Image
 
 def get_directory_names():
     directory_names = []
-    for picture in os.listdir("pictures/"):
+    for picture in os.listdir("Pictures/"):
         if picture:
-            directory_names.append("pictures/" + picture)
+            directory_names.append("Pictures/" + picture)
 
     return directory_names
-
 
 pictures = get_directory_names()
 
 # The first picture, because the [0] element is .DS_Store
+# TODO: REGEX for pictures
 
 base_picture = Image.open(pictures[1])
 base_width_height = base_picture.size
 
+page_title = pictures[0][0:8]
+
+width = str(base_width_height[0] * 0.2)
+
 print base_width_height
 
-html = ""
+html = "<center><h1>" + page_title + "</h1></center>"
 
 for src in get_directory_names():
-    if src != "pictures/.DS_Store":
-        html += '<img src="' + src + '">' + "\n"
+	if src != "Pictures/.DS_Store":
+		picture_name = src[9:]
+		picture_name =  picture_name[0:len(picture_name) - 4]
+        	html += '<center><img src="' + src + '" width =' + width + ' ></center> <center><h2>' + picture_name + '</center></h2><br>' + "\n"
 
 f = open("index.html", "w")
 
